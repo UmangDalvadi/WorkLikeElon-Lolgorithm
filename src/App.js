1e679331fbdceed9c61b7bbaac260ee563f2bf38
Umang Dalvadi <umangdalvadi8@gmail.com>
2023-10-15 00:36:16 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 import Quotes from './components/quotes'
+import Navbar from './components/navbar'
+import WebBlocker from './components/WebBlocker';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+
 
 
 function App() {
   return (
     <>
-      <Quotes />
+
+      <Router>
+        <Navbar />
+        <Quotes/>
+        <Routes>
+
+        {/* <Route exact path="/" element={<Quotes/>} /> */}
+        <Route exact path="/WebBlocker" element={<WebBlocker/>} />
+
+        </Routes>
+      </Router>
+      
+
     </>
   );
 }
--- src/components/quotes.js
+++ src/components/quotes.js
@@ -7,18 +7,23 @@ const quotes = () => {
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [data, setData] = useState([]);
-    const api_url = "https://api.quotable.io/random";
+    // const api_url = "https://api.quotable.io/random";
 
-    const api_fetch = async (url) => {
-        const res = await fetch(url);
-        const data = await res.json();
-        // console.log(data.content);
-        setData(data);
+    const api_fetch = () => {
+        fetch("https://api.quotable.io/random")
+            .then((res) => res.json())
+            .then((data) => setData(data))
     };
+    // const api_fetch = async (api_url) => {
+    //     const res = await fetch(api_url);
+    //     const data = await res.json();
+    //     // console.log(data.content);
+    //     setData(data);
+    // };
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
-        api_fetch(api_url);
+        api_fetch();
     }, []);
 
 
@@ -28,10 +33,14 @@ const quotes = () => {
                 <h2>Quote of the day</h2>
                 <blockquote >{data.content}</blockquote>
                 <span >{data.author}</span>
-                {/* <div>
-                    <button  >New Quote</button>
-                    <button>Tweet</button>
-                </div> */}
+                <div>
+                    <button onClick={api_fetch} >New Quote</button>
+                    <button>
+                        <a href={`https://twitter.com/intent/tweet?text=${data.content}`} target='_blank' rel="noreferrer noopener">
+                            Tweet
+                        </a>
+                    </button>
+                </div>
 
             </div>
 
